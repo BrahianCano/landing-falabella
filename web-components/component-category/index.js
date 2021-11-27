467b4c1b161f556d0bb2121965187b89f4d9369b
brahian <brahian.stiben@talent.com>
2021-11-26 21:25:12 -0500

    initial commit

---
+++ web-components/component-category/index.js
@@ -0,0 +1,35 @@
+class ComponentCategory extends HTMLElement {
+  constructor() {
+    super();
+    this.shadow = this.attachShadow({ mode: "open" });
+    this.state = {
+      name: "",
+      img: "",
+    };
+  }
+
+  static get observedAttributes() {
+    return ["name", "img"];
+  }
+
+  attributeChangedCallback(prop, oldVal, newVal) {
+    this.state = { ...this.state, [prop]: newVal };
+  }
+
+  connectedCallback() {
+    this.render();
+  }
+
+  render() {
+    this.shadow.innerHTML = `
+      <link href="web-components/component-category/styleguide.css" rel="stylesheet">  
+      
+      <div class="category-card">
+        <img src="${this.state.img}"/>
+        <div>
+          <h3>${this.state.name}</h3>
+        </div>
+      </div>`;
+  }
+}
+window.customElements.define("fblla-category", ComponentCategory);
